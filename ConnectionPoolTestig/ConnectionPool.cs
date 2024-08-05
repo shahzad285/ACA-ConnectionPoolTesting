96e4e9e196fb5eb578dc39fbbf4a4b12befa03d3
DESKTOP-S59BJJM\personal <shahzadahamad285@gmail.com>
2024-08-05 07:17:59 +0530

    Changes to test connection pooling

---
+++ ConnectionPoolTestig/ConnectionPool.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionPoolTestig
+{
+    internal class ConnectionPool
+    {
+        public static void TestConnectionWithoutPoolingAsync(string connectionString)
+        {
+            int connectionCount = 1000; // Number of connections to simulate
+            
+            for (int i = 0; i < connectionCount; i++)
+            {
+                Console.WriteLine("Connection " + i);
+
+                try
+                {
+                    var connection = new SqlConnection(connectionString);
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT 1", connection);
+                    command.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    break; // Stop the loop when an error occurs
+                }
+            }
+
+            Console.WriteLine("Test completed.");
+        }
+
+        public static void TestConnectionPoolingAsync(string connectionString)
+        {
+            int connectionCount = 1000; // Number of connections to simulate
+
+            Queue<SqlConnection> queue = new Queue<SqlConnection>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var connection = new SqlConnection(connectionString);
+                connection.Open();
+                queue.Enqueue(connection);
+
+            }
+
+            for (int i = 0; i < connectionCount; i++)
+            {
+                Console.WriteLine("Connection " + i);
+
+                try
+                {
+                    var connection =queue.Dequeue();
+                    SqlCommand command = new SqlCommand("SELECT 1", connection);
+                    command.ExecuteScalar();
+                    
+                    queue.Enqueue(connection);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    break; // Stop the loop when an error occurs
+                }
+            }
+
+            Console.WriteLine("Test completed.");
+        }
+    }
+}
---
+++ ConnectionPoolTestig/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConnectionPoolTestig;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+// Run the test with pooling enabled
+Console.WriteLine("Testing database connection without pooling");
+//ConnectionPool.TestConnectionWithoutPoolingAsync(connectionString);
+
+Console.WriteLine("Testing database connection with pooling");
+ConnectionPool.TestConnectionPoolingAsync(connectionString);
